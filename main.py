fd56b1443691d4ee16492dc553690da222cb8a4f
Joshua Bueno <jbueno@buffalo.edu>
2014-07-16 12:40:10 -0400

    changed old README to data_README and created new README specifically for the project

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import ratings_base
 import user_base
 import netflix_objects
 import heapq
-import random
 from random import randint
 
 movieBase=movie_base.parse_moviebase()
@@ -75,24 +74,5 @@ def print_topk_all(j,n,k): # j = total number of users, n = number of users we w
 			topMoviePos=topMoviePos+1
 print_topk_all(943,3,50)
 
-'''
-unknown|0
-Action|1
-Adventure|2
-Animation|3
-Children's|4
-Comedy|5
-Crime|6
-Documentary|7
-Drama|8
-Fantasy|9
-Film-Noir|10
-Horror|11
-Musical|12
-Mystery|13
-Romance|14
-Sci-Fi|15
-Thriller|16
-War|17
-Western|18
-'''
+
+
